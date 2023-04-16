72057baba13cfe4ebd255fa06441e649281cdb73
omer1325 <omer.ingec24@gmail.com>
2023-04-17 02:14:24 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://yjdwxurs:wN1C__2I3RQBa7YFNIZD8YB7HY2Ddl3w@woodpecker.rmq.cloudamqp.com/yjdwxurs");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://yjdwxurs:wN1C__2I3RQBa7YFNIZD8YB7HY2Ddl3w@woodpecker.rmq.cloudamqp.com/yjdwxurs");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+Console.Read();
